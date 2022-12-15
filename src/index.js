86164f093499177b2ac8d31b40cf09b3408de69c
mohamedmehdigara <garamehdi57@yahoo.com>
2022-12-15 15:40:01 +0100

    added files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import GameLoop from "./GameLoop";
-import GameEngine from "./GameEngine";
 import DefaultRenderer from "./DefaultRenderer";
 import DefaultTimer from "./DefaultTimer";
+import React, { PureComponent } from "react";
+import { GameEngine } from "react-game-engine";
+import { Box } from "./renderers";
+import { MoveBox } from "./systems"
+
 
 export {
 	GameLoop,
@@ -11,3 +15,20 @@ export {
 	DefaultTimer,
 	DefaultTimer as Timer
 };
+export default class SimpleGame extends PureComponent {
+	render() {
+	  return (
+		<GameEngine
+		  style={{ width: 800, height: 600, backgroundColor: "blue" }}
+		  systems={[MoveBox]}
+		  entities={{
+			//-- Notice that each entity has a unique id (required)
+			//-- and a renderer property (optional). If no renderer
+			//-- is supplied with the entity - it won't get displayed.
+			box1: { x: 200,  y: 200, renderer: <Box />}
+		  }}>
+  
+		</GameEngine>
+	  );
+	}
+  }
